908b6e48efd437ac2e4ccfb12d6b82dd80478423
unknown <A185@INLTD09583.global.tesco.org>
2015-11-30 15:52:57 +0530

    Externalizing configuration

--- exercise1/customer-rewards/src/main/java/com/dwl/services/App.java
+++ exercise1/customer-rewards/src/main/java/com/dwl/services/App.java
@@ -1,24 +1,25 @@
 package com.dwl.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.dropwizard.Application;
-import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class App extends Application<Configuration> {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App extends Application<CustomerRewardsConfiguration> {
 	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
 	@Override
-	public void initialize(Bootstrap<Configuration> b) {
+	public void initialize(Bootstrap<CustomerRewardsConfiguration> b) {
 	}
 
 	@Override
-	public void run(Configuration c, Environment e) throws Exception {
+	public void run(CustomerRewardsConfiguration c, Environment e)
+			throws Exception {
 		LOGGER.info("Method App#run() called");
 		System.out.println("Hello world, by Dropwizard!");
+		System.out.println("Coucbase Bucket : " + c.getCouchbaseBucket());
 	}
 
 	public static void main(String[] args) throws Exception {
